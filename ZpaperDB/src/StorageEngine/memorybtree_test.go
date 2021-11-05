883d88d468e9f3ced775ea2b5d7b44180cbc6084
Xbzzy <57384383+Xbzzy@users.noreply.github.com>
2021-11-05 21:36:05 +0800

    Update memorybtree_test.go

--- ZpaperDB/src/StorageEngine/memorybtree_test.go
+++ ZpaperDB/src/StorageEngine/memorybtree_test.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 )
 
-func TestCreateBTree() (*storage.BTree,error) {
-	btree:=new(storage.BTree)
+func TestCreateBTree() (tree *BTree,error) {
+	btree:=new(BTree)
 	_=btree.InitBTree(3,"test.txt")
 	for i:=1;i<500;i++ {
 		TmpIndex:=btree.CreateIndex(byte(i),"")
 		btree.Insert(TmpIndex)
 	}
 	return btree,nil
+}
+func TestInsertBTree() (tree *BTree,error) {
+	return tree,nil
 }
